9853cf84032d55374648a637cf16c86967dbbfc3
Arek Paprzycki <arek.working@gmail.com>
2020-05-17 11:17:57 +0200

    Immutability at objects

--- 2st-main.js
+++ 2st-main.js
@@ -35,6 +35,54 @@ const numberTwo = 25
 const nameOne = 'Arek'
 const nameTwo = 'Maciek'
 
+
+/* object 1 */
+const carOne = {
+    name: 'mercedes15',
+    productionDate: '2014r',
+    color: 'white',
+    engine: {
+        capacity: 2.2,
+        power: '215 hp',
+    },
+    isTurboOn: true,
+    alarm: false,
+    racing: (opponent) => {
+        console.log(`I'll be racing with ${opponent}`)
+    },
+}
+
+carOne.racing('Vin Disel')
+
+/* object 2 */
+const carTwo = {
+    name: 'mercedes15',
+    productionDate: '2014r',
+    color: carOne.color,
+    engine: {
+        capacity: 2.2,
+        power: '215 hp',
+    },
+    isTurboOn: true,
+    alarm: false,
+    racing: (opponent) => {
+        console.log(`I'll be racing with ${opponent}`)
+    },
+}
+
+carTwo.engine.capacity = 'only 100 hp'
+carTwo.name = 'porshe'
+carTwo.engine.capacity = carOne.engine.capacity
+carTwo.engine.capacity = 'hello hp'
+
+carOne.racing('Vin Disel')
+
+carOne.color = 'black'
+
+console.log(carOne)
+/* console.log(carOne.engine.capacity) */
+console.log(carTwo)
+
 const humanOne = {
     name: 'Maciek',
     age: 32,
