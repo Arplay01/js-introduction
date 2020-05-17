da314e0f1020a66c0edf1d5292ec2b9f32794041
Arek Paprzycki <arek.working@gmail.com>
2020-05-17 10:33:29 +0200

    simple excercises with js - functions & objects

---
+++ 2st-main.js
@@ -0,0 +1,46 @@
+
+/* alert('Hello in my World') */
+/* console.log('Im practising JS)')
+
+const name = 'Basia'
+let age = 56
+const newAge = age+10
+
+console.log(`age is ${age}`);
+console.log(`newAge is ${newAge}`);
+
+const heading = document.querySelector('.welcome')
+
+console.log(heading)
+
+const header = document.querySelector('.main-heading--js')
+
+header.innerHTML = `Hello name is ${name}`
+
+function policzToSam(kolejnyAge) {
+    console.log(`dostałem ${kolejnyAge}`);
+    return kolejnyAge*5;
+}
+
+const myResults = policzToSam(5);
+
+console.log(myResults)
+ */
+
+/*  IMMUTABILITY in Objects*/
+
+const numberOne = 15
+const numberTwo = 25
+
+const nameOne = 'Arek'
+const nameTwo = 'Maciek'
+
+const humanOne = {
+    name: 'Maciek',
+    age: 32,
+}
+
+const humanTwo = {
+    name: 'Arek',
+    age: 25,
+}
--- main.js
+++ main.js
@@ -23,6 +23,10 @@ console.log(age);
 console.log(myResult);
  */
 
+ const calculate2 = (myNumber) => myNumber*7
+ const myResult = calculate2(8)
+ console.log(myResult)
+
 /* const heading = document.querySelector('.welcome');
 console.log(heading); */
 
@@ -33,7 +37,46 @@ console.log(heading.innerHTML);
 heading.innerHTML = `<span style="color: yellow;">Experiments</span> ${age}`;
 console.log(heading.innerHTML);
 
+/* function greet(myNumber) {
+    console.log(`dostałem ${myNumber}`);
+    myNumber = myNumber*7;
+    return `Hello ${myNumber}`;
 
+}
+
+const myResult = calculate(age);
+
+console.log(myResult) */
+
+/* function greet(age, name) { 
+    heading.innerHTML = `<span style="color: yellow;">Experiments</span> To jest mój wiek:${age}, a tak mam na imię: ${name}`;
+    console.log(heading.innerHTML);
+}
+greet(18, 'Arek' )*/
+
+const greet = (age, name) => {
+    heading.innerHTML = `Experiments To jest mój wiek:${age}, a tak mam na imię: ${name}`;
+    console.log(heading.innerHTML);
+}
+greet(40,'arek')
+
+ function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+ }
+
+ createContent('.welcome__heading--js', 'Witam w nowym Jorku <span style="color: yellow">elo</span> elo')
+
+ createContent('.main-heading--js', 'Hello<span style="color: yellow">World</span> elo')
+
+
+ console.log()
+
+ function helloWorld() {
+     console.log('Dzień dobry :D 😎')
+ }
+
+ helloWorld()
 
 
 
